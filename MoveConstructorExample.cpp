1eee3d6a2929cdeff5f9b87ac157c327120b0bad
Erik Polderman <eriksnoww@gmail.com>
2019-09-21 16:52:33 -0600

    Clearing un-needed tracked files

---
+++ MoveConstructorExample.cpp
@@ -0,0 +1,14 @@
+#include "pch.h"
+#include "MoveConstructorExample.h"
+
+
+MoveConstructorExample::MoveConstructorExample()
+{
+	cout << "Constructor" << endl;
+}
+
+
+MoveConstructorExample::~MoveConstructorExample()
+{
+	cout << "Destructor" << endl;
+}
